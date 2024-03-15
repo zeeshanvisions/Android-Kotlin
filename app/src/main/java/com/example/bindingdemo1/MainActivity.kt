e8fca754e685deb44f978056414824b863df444a
Zeeshan ahmed <zeeshanvisions@gmail.com>
2024-03-15 15:52:52 +0500

    Live Data with Binding

--- app/src/main/java/com/example/bindingdemo1/MainActivity.kt
+++ app/src/main/java/com/example/bindingdemo1/MainActivity.kt
@@ -6,22 +6,41 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.bindingdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.submitButton.setOnClickListener {
-            displayGreeting()
-        }
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+//        viewModel.startingCount.observe(this, Observer {
+//            binding.greetingTextView.text = it.toString()
+//        })
+
+//        binding.submitButton.setOnClickListener {
+//            viewModel.increment()
+//        }
+    }
+}
+
+class MainActivityViewModel: ViewModel() {
+    var startingCount = MutableLiveData<Int>()
+
+    init {
+        startingCount.value = 0
     }
 
-    private fun displayGreeting() {
-        binding.apply {
-            greetingTextView.text = "Hello! "+ nameEditText.text
-        }
+    fun increment() {
+        startingCount.value = (startingCount.value)?.plus(1)
     }
 }
